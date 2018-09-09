3eb3f731517a751397291b119c38fc9edc54f6a0
jf5577 <jfempire@yahoo.co.uk>
2018-09-09 15:12:04 +0530

    vue onboarded

---
+++ src/dctool.vue
@@ -0,0 +1,37 @@
+<template>
+<div>hi{{a}}</div>    
+</template>
+
+<script>
+/*function tv(stuff) {
+  document.getElementById("tv").innerHTML = "";
+  document.getElementById("tv").innerHTML = stuff;
+}
+tv("test TV");
+function con(stuff) {
+  document.getElementById("console").innerHTML += `<br>` + stuff;
+}
+con("test CON");
+
+//Array for output
+let N = [[0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0], [0, 0, 0]]; //rows=>Rack position,columns=>Server Type
+//Arrays for Input
+let ServerLabel = ["S1", "S2", "S3"],
+  ServerPower = [1000, 2000, 4000],
+  ServerSpace = [1, 2, 4],
+  ServerCount = [7, 5, 3],
+  RackLabel = ["R1", "R2", "R3", "R4", "R5"],
+  RackPower = [10000, 9000, 7000, 5000, 3000],
+  RackSpace = [10, 9, 7, 5, 3];
+
+function showOutput() {}
+*/
+export default {
+  data() {
+    return { a: "a" };
+  }
+};
+</script>
+
+<style>
+</style>
---
+++ src/script.js
@@ -0,0 +1,3 @@
+import Vue from "vue";
+import dctool from "./dctool";
+new Vue({ el: "#app", render: h => h(dctool) });
